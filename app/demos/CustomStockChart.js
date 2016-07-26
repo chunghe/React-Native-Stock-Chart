6ff74fe204c9a82006b2e46d988f890d5e8ef554
chunghe <chunghe@gmail.com>
2016-07-26 13:40:07 +0800

    draw tick component

--- app/demos/CustomStockChart.js
+++ app/demos/CustomStockChart.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { ScrollView, StyleSheet, Dimensions } from 'react-native';
-import Svg, { Path, Text } from 'react-native-svg';
+import Svg, {G,  Path, Text } from 'react-native-svg';
 import data from '../data';
 
 import * as d3Shape from 'd3-shape';
@@ -30,6 +30,10 @@ class CustomStockChart extends Component {
         .range([0, defaultHeight].reverse());
   }
 
+  roundDecimal(n, decimal = 2) {
+    return Math.round(n * 100) / 100;
+  }
+
   getStockPath() {
     const { ticks, lowestPrice, highestPrice, tradingHours } = data;
     const { xScale, yScale } = this;
@@ -44,27 +48,30 @@ class CustomStockChart extends Component {
 
 
   getTickValues() {
-    const { ticks, lowestPrice, highestPrice, tradingHours } = data;
+    const { ticks, lowestPrice, highestPrice, tradingHours, previousClose } = data;
     const yTicks = this.yScale.ticks(5);
     const xScaled = tradingHours.map( t => this.xScale(t * 1000));
+    const roundDecimal = this.roundDecimal;
     return yTicks.map(tick => {
       const yScaled = this.yScale(tick);
       return {
         x1: xScaled[0],
         y1: yScaled,
         x2: xScaled[1],
-        y2: yScaled
+        y2: yScaled,
+        tick: '' + tick,
+        percent: roundDecimal((tick - previousClose) * 100/ previousClose)
       };
     })
   }
 
   getTickLabel() {
 
+    const values = this.getTickValues();
+
   }
 
-  getTickPath() {
-    const values = this.getTickValues();
-    console.log('values', values);
+  getTickPath(values) {
     return values.reduce((prev, current) => {
       return `${prev} M${current.x1} ${current.y1} ${current.x2} ${current.y2}`;
     }, '');
@@ -74,15 +81,24 @@ class CustomStockChart extends Component {
   render() {
     const { ticks } = data;
     const path = this.getStockPath();
-    console.log('->', this.getTickPath());
+    const values = this.getTickValues();
+    console.log('values', values);
 
     return (
       <ScrollView style={styles.container}>
         <Svg height={defaultHeight} width={defaultWidth}>
           <Path d={path} stroke="rgb(70, 171, 209)" strokeWidth={1.5} fill="none" />
-          <Path d={this.getTickPath()} stroke="rgb(153, 153, 153)" />
-          <Text x="0" y="31.633954857701298" style={{fill: '#252525', fontSize: 14, stroke: 'transparent', backgroundColor: '#d9d9d9'}}>8860</Text>
-          <Text x="0" y="65.98135426888949" style={{fill: '#252525', fontSize: 14, stroke: 'transparent', backgroundColor: '#d9d9d9'}}>8860</Text>
+          <Path d={this.getTickPath(values)} stroke="rgb(153, 153, 153)" />
+          <G>
+          {
+            values.map((value, index) => {console.log('value', value); return <Text key={index} x={value.x1} y={value.y1}>{value.tick}</Text>})
+          }
+          </G>
+          <G>
+          {
+            values.map((value, index) => {console.log('value', value); return <Text key={index} x={value.x2} y={value.y1} textAnchor="end">{`${value.percent}%`}</Text>})
+          }
+          </G>
         </Svg>
       </ScrollView>
     );
