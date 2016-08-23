73e9599ea88cda8cbd53bdcd74b652c26832e7c2
chunghe <chunghe@gmail.com>
2016-08-23 17:48:45 +0800

    able to load more

--- app/demos/CandleStick.js
+++ app/demos/CandleStick.js
@@ -27,12 +27,42 @@ class CandleStick extends Component {
   }
 
   getStockQuotes = () => {
-    const url = 'http://hulk.dev.cnyes.cool/api/v1/history?symbol=2330&from=1467302400&to=1471536000&resolution=D';
+    const d = new Date();
+    const today = new Date(`${d.getFullYear()}/${d.getMonth() + 1}/${d.getDate()}`).getTime();
+    const from = today + 86400 * 1000;
+    const to = from - 86400 * 30 * 1000;
+    // const url = `http://hulk.dev.cnyes.cool/api/v1/history?symbol=2330&from=1467302400&to=1471536000&resolution=D`;
+    const url = `http://hulk.dev.cnyes.cool/api/v1/history?symbol=2330&from=${Math.floor(from/1000)}&to=${Math.floor(to/1000)}&resolution=D`;
+    console.log('url', url);
     fetch(url)
       .then(rsp => rsp.json())
       .then(data => {
-        this.setState({ data, current: -1 });
+        this.setState({ data, current: 0 });
       });
+  }
+
+  loadMore = () => {
+    const { nextTime } = this.state.data;
+    const to = (nextTime * 1000- 86400 * 30 * 1000) / 1000;
+    if (nextTime) {
+      const url = `http://hulk.dev.cnyes.cool/api/v1/history?symbol=2330&from=${nextTime}&to=${Math.floor(to)}&resolution=D`;
+      fetch(url)
+        .then(rsp => rsp.json())
+        .then(data => {
+          const originData = this.state.data;
+          const newData = {
+            ...originData,
+            ...data,
+            c: [...originData.c, ...data.c],
+            h: [...originData.h, ...data.h],
+            l: [...originData.l, ...data.l],
+            o: [...originData.o, ...data.o],
+            t: [...originData.t, ...data.t],
+            v: [...originData.v, ...data.v],
+          }
+          this.setState({data: newData});
+        });
+    }
   }
 
   getLinearScale(domain, range, isTime = false) {
@@ -50,7 +80,7 @@ class CandleStick extends Component {
       t: new Date(t[i] * 1000),
       v: v[i],
       s,
-      color: (current === i ? '#000': (o[i] <= c[i] ? 'red' : 'green'))
+      color: o[i] <= c[i] ? 'rgb(210, 72, 62)' : 'rgb(28, 193, 135)'
     };
   }
 
@@ -90,10 +120,10 @@ class CandleStick extends Component {
         <T heading>CandleStick chart</T>
         <Text>{`時間: ${new Date(t[current] * 1000)}`}</Text>
         <View style={{ flexDirection: 'row' }}>
+          <Text style={{ flex: 1 }}>{`收盤: ${c[current]}`}</Text>
+          <Text style={{ flex: 1 }}>{`開盤: ${o[current]}`}</Text>
           <Text style={{ flex: 1 }}>{`最高: ${h[current]}`}</Text>
           <Text style={{ flex: 1 }}>{`最低: ${l[current]}`}</Text>
-          <Text style={{ flex: 1 }}>{`開盤: ${o[current]}`}</Text>
-          <Text style={{ flex: 1 }}>{`收盤: ${c[current]}`}</Text>
         </View>
         <Svg
           height={defaultStockChartHeight}
@@ -117,11 +147,15 @@ class CandleStick extends Component {
                   <Rect
                     x={x}
                     y={Math.min(scaleO, scaleC)}
-                    fill={current === i ? 'blue': color}
+                    fill={color}
                     height={barHeight}
                     width={barWidth}
                   />
-                  <Path stroke={color} d={`M${x + barWidth / 2} ${yTop} L${x + barWidth / 2} ${yBottom}`} strokeWidth="1" />
+                  <Path stroke={color} d={`M${x + barWidth / 2} ${yTop} ${x + barWidth / 2} ${yBottom}`} strokeWidth="1" />
+                  {current === i && <Path stroke="#666" d={`M${x + barWidth / 2} 0 ${x + barWidth / 2} ${defaultStockChartHeight}`} strokeWidth="0.5" />}
+                  {current === i &&
+                  <Path stroke="#666" d={`M0 ${scaleC} ${deviceWidth} ${scaleC}`} strokeWidth="0.5" />
+                  }
                 </G>
               );
             }, this)
@@ -147,9 +181,12 @@ class CandleStick extends Component {
           }
           </G>
         </Svg>
-        <View style={{ padding: 15 }}>
+        <View style={{ padding: 15, flexDirection: 'row' }}>
           <TouchableOpacity style={styles.button} onPress={this.toggleGridline}>
             <Text>toggle grid line</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button} onPress={this.loadMore}>
+            <Text>load more</Text>
           </TouchableOpacity>
         </View>
       </ScrollView>
@@ -166,8 +203,8 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     borderColor: '#666',
     borderStyle: 'solid',
-    position: 'absolute', // not occupying full width
     padding: 10,
+    marginRight: 10
   }
 });
 
