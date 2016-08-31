ad9ab0e1d27221829187104b0ceae37980c13753
chunghe <chunghe@gmail.com>
2016-08-31 14:20:47 +0800

    able to distinguish between tap-and-hold and swipe gesture

--- app/demos/PanResponderOverlay.js
+++ app/demos/PanResponderOverlay.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { PanResponder, View, Dimensions,  ScrollView, StyleSheet } from 'react-native';
+import { PanResponder, Text, View, Dimensions,  ScrollView, StyleSheet } from 'react-native';
 
 import Svg, { G, Text as SvgText, Rect } from 'react-native-svg';
 
@@ -16,6 +16,7 @@ class PanResponderOverlay extends Component {
     this.state = {
       data: {},
       showGridline: false,
+      scrollEnabled: true
     };
     this._previousOffset = 0;
     this.elPanStyle = {
@@ -47,48 +48,108 @@ class PanResponderOverlay extends Component {
   _alwaysTrue = () => true
 
   _handlePanResponderMove = (e, gestureState) => {
+    console.log('move');
     this.elPanStyle.style.transform[0].translateX = this._previousOffset + gestureState.dx;
     this._updateNativeStyles();
   }
 
   _handlePanResponderEnd = (e, gestureState) => {
+    console.log('end');
     this._previousOffset += gestureState.dx;
   }
-
 
   _updateNativeStyles = () => {
     this.elPan && this.elPan.setNativeProps(this.elPanStyle);
   }
 
 
+  handleTouchStart = (e) => {
+    console.log('handleTouchStart', e.nativeEvent);
+    const { locationX, locationY, timestamp } = e.nativeEvent;
+    this._touchStartTime = timestamp;
+    this._touchStartPosition = {
+      locationX, locationY
+    };
+  }
+
+  // move less than 1 px
+  closeEnough(p1, p2) {
+    if (Math.abs(p1 - p2) < 2) {
+      return true;
+    }
+    return false;
+  }
+
+  handleTouchMove = (e) => {
+    console.log('handleTouchMove');
+    const { timestamp, locationX, locationY } = e.nativeEvent;
+    if (this.state.scrollEnabled !== false) {
+      if (timestamp - this._touchStartTime > 300) {
+        console.log('check position', e.nativeEvent)
+        if (this.closeEnough(locationX, this._touchStartPosition.locationX) &&
+            this.closeEnough(locationY, this._touchStartPosition.locationY)) {
+          console.log('-- lock --');
+          this.setState({scrollEnabled: false});
+        }
+      }
+    }
+  }
+
+  handleTouchEnd = () => {
+    console.log('handleTouchEnd');
+    if (this.state.scrollEnabled === false) {
+      this.setState({scrollEnabled: true});
+    }
+  }
+
+  handleScrollBeginDrag = () => {
+    console.log('handleScrollBeginDrag');
+  }
+
+  handleScrollEndDrag = () => {
+    console.log('handleScrollEndDrag');
+  }
+
   render() {
     const svgWidth = this.getSvgWidth();
     const points = d3Array.range(0, svgWidth, 50);
 
+    // scroll view 的 onTouchMove event fire 很正常
+    // TouchWithoutFeedback/svg 的 panhandelr 常常會斷掉 (move -> end)
     return (
-      <ScrollView style={styles.container}>
+      <View style={styles.container}>
         <T heading>PanResponder overlay</T>
-        <View
-          width={svgWidth}
-          height={defaultStockChartHeight + 10}
-          {...this._panResponder.panHandlers}
-          ref={elPan => { this.elPan = elPan; }}
+        <ScrollView
+          horizontal={true}
+          directionalLockEnabled={true}
+          scrollEnabled={this.state.scrollEnabled}
+          bounces={false}
+          onTouchStart={this.handleTouchStart}
+          onTouchMove={this.handleTouchMove}
+          onTouchEnd={this.handleTouchEnd}
+          onScrollBeginDrag={this.handleScrollBeginDrag}
+          onScrollEndDrag={this.handleScrollEndDrag}
+          style={{
+            height: defaultStockChartHeight,
+            width: deviceWidth,
+            backgroundColor:this.state.scrollEnabled ? 'green' : 'red'
+          }}
         >
-          <Svg
-            height={defaultStockChartHeight}
-            width={svgWidth}
-            style={{ backgroundColor: '#efefef' }}
-          >
-          {points.map(point =>
-            <G key={point}>
-              <Rect fill="red" width="2" height="10" x={point} y={defaultStockChartHeight / 2} />
-              <SvgText x={point} y={defaultStockChartHeight / 2 + 10} textAnchor="middle">{`${point}`}</SvgText>
-            </G>
-            )}
+            <Svg
+              height={defaultStockChartHeight}
+              width={svgWidth}
+              style={{ backgroundColor: '#efefef' }}
+            >
+            {points.map(point =>
+              <G key={point}>
+                <Rect fill="red" width="2" height="10" x={point} y={defaultStockChartHeight / 2} />
+                <SvgText x={point} y={defaultStockChartHeight / 2 + 10} textAnchor="middle">{`${point}`}</SvgText>
+              </G>
+              )}
 
-          </Svg>
-        </View>
-      </ScrollView>
+            </Svg>
+        </ScrollView>
+      </View>
     );
   }
 }
